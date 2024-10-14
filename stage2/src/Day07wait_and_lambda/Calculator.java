d873771d21a3f4043d2e90e06add51ff5f70ae82
ApexJava <116183621+ApexJava@users.noreply.github.com>
2024-10-14 17:26:27 +0800

    Delete stage2 directory
    误添加

--- stage2/src/Day07wait_and_lambda/Calculator.java
+++
@@ -1,5 +0,0 @@
-package Day07wait_and_lambda;
-
-public interface Calculator {
-    int calc(int a,int b);
-}
--- stage2/src/Day07wait_and_lambda/baozi.java
+++
@@ -1,7 +0,0 @@
-package Day07wait_and_lambda;
-
-public class baozi {
-     String pi;
-     String xian;
-    boolean state = false;
-}
